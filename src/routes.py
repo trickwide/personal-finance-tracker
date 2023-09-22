729657b8d44732ffb2f24550a2f8d986c5cfef11
Ville Saastamoinen <trickwide@gmail.com>
2023-09-22 19:23:35 +0300

    Added validation checks for username and password, added error messages to routes

--- src/routes.py
+++ src/routes.py
@@ -1,10 +1,10 @@
 from app import app
-from flask import Flask, render_template, redirect, request, flash, session
+from flask import Flask, render_template, redirect, request, flash, session, url_for
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql import text
 from werkzeug.security import generate_password_hash, check_password_hash
 from test_db import db
-from services.user_service import register_user
+from services.user_service import register_user, is_username_valid, is_password_valid
 
 
 @app.route("/")
@@ -26,12 +26,21 @@ def register():
 
         # Check if passwords match
         if password != password_confirm:
-            return "Passwords do not match", 400
+            flash("Passwords do not match")
+            return redirect(url_for('register'))
+
+        if not is_username_valid(username):
+            flash("Username is too short. Minimum length is 5 characters")
+            return redirect(url_for('register'))
+        if not is_password_valid(password):
+            flash("Password is too weak. It must be at least 8 characters long and contain an uppercase letter, a number and a special character")
+            return redirect(url_for('register'))
 
         try:
             register_user(username, password)
-            return redirect("/")
+            return redirect(url_for('index'))
         except ValueError as error:
             flash(str(error))
+            return redirect(url_for('register'))
 
     return render_template("frontend/register.html")
--- src/services/user_service.py
+++ src/services/user_service.py
@@ -1,6 +1,22 @@
 from sqlalchemy.sql import text
 from werkzeug.security import generate_password_hash, check_password_hash
 from test_db import db
+import re
+
+
+def is_username_valid(username):
+    return len(username) >= 5
+
+
+def is_password_valid(password):
+    if len(password) < 8:
+        return False
+
+    has_uppercase = any(c.isupper() for c in password)
+    has_number = any(c.isdigit() for c in password)
+    has_special = bool(re.search(r"[!@#$%^&*(),.?\":{}|<>]", password))
+
+    return has_uppercase and has_number and has_special
 
 
 def register_user(username, password):
